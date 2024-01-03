65608938b744ab052cdb2d1f0ce52566f8b9f5e1
Mozart Rueckert <mozartrue@outlook.com>
2024-01-02 23:58:03 -0300

    feat: set app colors

---
+++ lib/core/ui/consts/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color slate900 = Color(0xff0F172A);
+  static const Color slate800 = Color(0xff1E293B);
+  static const Color slate700 = Color(0xff334155);
+  static const Color slate600 = Color(0xff475569);
+  static const Color slate500 = Color(0xff64748B);
+  static const Color slate400 = Color(0xff94A3B8);
+  static const Color slate300 = Color(0xffCBD5E1);
+  static const Color slate200 = Color(0xffE2E8F0);
+  static const Color slate100 = Color(0xffF1F5F9);
+
+  static const Color emerald700 = Color(0xff047857);
+  static const Color red700 = Color(0xffB91C1C);
+}
