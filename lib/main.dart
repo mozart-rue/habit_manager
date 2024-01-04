369c06dcf571ce14973b4ceea671899e4f5d67ad
Mozart Rueckert <mozartrue@outlook.com>
2024-01-03 23:39:59 -0300

    feat: set initial route to login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_manager/core/ui/pages/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: Container(),
+      home: const LoginScreen(),
     );
   }
 }
