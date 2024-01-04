296233b119c8356ed1c78ad17f9d975d6e35a8d5
Mozart Rueckert <mozartrue@outlook.com>
2024-01-03 23:39:25 -0300

    feat: create background screen

---
+++ lib/core/ui/pages/background/background_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:habit_manager/core/ui/consts/app_colors.dart';
+
+class AppBackgroundScreen extends StatelessWidget {
+  final Widget child;
+  const AppBackgroundScreen({required this.child, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height,
+            width: size.width,
+            color: AppColors.slate700,
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
