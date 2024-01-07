0e511726e14caf05a75e9c7cbf2bb9b96b8535ff
Mozart Rueckert <mozartrue@outlook.com>
2024-01-06 23:45:09 -0300

    fix: changed colorscheme

--- lib/core/ui/consts/app_colors.dart
+++ lib/core/ui/consts/app_colors.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color slate900 = Color(0xff0F172A);
-  static const Color slate800 = Color(0xff1E293B);
-  static const Color slate700 = Color(0xff334155);
-  static const Color slate600 = Color(0xff475569);
-  static const Color slate500 = Color(0xff64748B);
-  static const Color slate400 = Color(0xff94A3B8);
-  static const Color slate300 = Color(0xffCBD5E1);
-  static const Color slate200 = Color(0xffE2E8F0);
-  static const Color slate100 = Color(0xffF1F5F9);
+  static const Color slate900 = Color(0xff1C1917);
+  static const Color slate800 = Color(0xff292524);
+  static const Color slate700 = Color(0xff44403C);
+  static const Color slate600 = Color(0xff57534E);
+  static const Color slate500 = Color(0xff78716C);
+  static const Color slate400 = Color(0xffA8A29E);
+  static const Color slate300 = Color(0xffD6D3D1);
+  static const Color slate200 = Color(0xffE7E5E4);
+  static const Color slate100 = Color(0xffF5F5F4);
 
-  static const Color emerald700 = Color(0xff047857);
+  static const Color blue = Color(0xff1A8AE5);
   static const Color red700 = Color(0xffB91C1C);
 
   static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffFFFFFF);
 }
