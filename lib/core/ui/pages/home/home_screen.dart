ed6a659dd1fc25bf87befbdb2be40876293aad87
Mozart Rueckert <mozartrue@outlook.com>
2024-01-09 21:24:49 -0300

    feat: add scroll view to page

--- lib/core/ui/pages/home/home_screen.dart
+++ lib/core/ui/pages/home/home_screen.dart
@@ -20,92 +20,94 @@ class HomeScreen extends StatelessWidget {
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          child: Column(
-            children: [
-              const DisplayCurrentDateComponent(),
-              const Gap(8),
-              Row(
-                children: [
-                  const Text(
-                    'Olá, ',
-                    style: TextStyle(
-                      color: AppColors.slate100,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    userName,
-                    style: const TextStyle(
-                      color: AppColors.blue,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
-              ),
-              const Gap(20),
-              Container(
-                width: size.width * 0.9,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: AppColors.blue,
-                  gradient: const LinearGradient(
-                    colors: [
-                      AppColors.blue,
-                      AppColors.lightBlue,
-                      AppColors.lightestBlue,
-                    ],
-                    stops: [0.20, 0.45, 0.75],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const HabitCircularProgressComponent(),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '$dailyHabitsDone de $totalDailyHabits Habitos',
-                            style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w500,
-                              color: AppColors.slate100,
-                            ),
-                          ),
-                          const Text(
-                            'concluídos hoje',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                              color: AppColors.slate100,
-                            ),
-                          ),
-                        ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const DisplayCurrentDateComponent(),
+                const Gap(8),
+                Row(
+                  children: [
+                    const Text(
+                      'Olá, ',
+                      style: TextStyle(
+                        color: AppColors.slate100,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600,
                       ),
-                    ],
+                    ),
+                    Text(
+                      userName,
+                      style: const TextStyle(
+                        color: AppColors.blue,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                const Gap(20),
+                Container(
+                  width: size.width * 0.9,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: AppColors.blue,
+                    gradient: const LinearGradient(
+                      colors: [
+                        AppColors.blue,
+                        AppColors.lightBlue,
+                        AppColors.lightestBlue,
+                      ],
+                      stops: [0.20, 0.45, 0.75],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const HabitCircularProgressComponent(),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$dailyHabitsDone de $totalDailyHabits Habitos',
+                              style: const TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500,
+                                color: AppColors.slate100,
+                              ),
+                            ),
+                            const Text(
+                              'concluídos hoje',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: AppColors.slate100,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const Gap(30),
-              HeaderDisplayTopItensComponent(
-                title: 'Habitos do dia',
-                onTapSeeAll: () {},
-                children: const [
-                  Gap(12),
-                  HabitTileComponent(done: true),
-                  Gap(8),
-                  HabitTileComponent(done: true),
-                  Gap(8),
-                  HabitTileComponent(),
-                ],
-              ),
-            ],
+                const Gap(30),
+                HeaderDisplayTopItensComponent(
+                  title: 'Habitos do dia',
+                  onTapSeeAll: () {},
+                  children: const [
+                    Gap(12),
+                    HabitTileComponent(done: true),
+                    Gap(8),
+                    HabitTileComponent(done: true),
+                    Gap(8),
+                    HabitTileComponent(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
