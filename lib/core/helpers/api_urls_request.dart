0950f5ca294d8fc416211e313b33810de681237a
mozart-rue <mozartrue@outlook.com>
2024-02-19 22:00:39 -0300

    feat: add config to centralize api endpoints

---
+++ lib/core/helpers/api_urls_request.dart
@@ -0,0 +1,8 @@
+import 'package:habit_manager/core/env/environment.dart';
+
+class ApiUrlsRequest {
+ ApiUrlsRequest();
+
+ // validar metodo env para 
+ static final String apiUrl = Environment().apiUrl;
+}
