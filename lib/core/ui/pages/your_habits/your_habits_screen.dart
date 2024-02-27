8f7dce19eaebbbb4889071b12c552a5a371195d2
mozart-rue <mozartrue@outlook.com>
2024-02-26 21:51:25 -0300

    feat: Add Icon to empty list
    Add icon that display when list of habits is empty.

--- lib/core/ui/pages/your_habits/your_habits_screen.dart
+++ lib/core/ui/pages/your_habits/your_habits_screen.dart
@@ -4,6 +4,7 @@ import 'package:gap/gap.dart';
 import 'package:habit_manager/core/data/models/habit_model.dart';
 import 'package:habit_manager/core/data/services/fetch_habits_service.dart';
 import 'package:habit_manager/core/ui/components/animated_loading.dart';
+import 'package:habit_manager/core/ui/components/empty_space.dart';
 import 'package:habit_manager/core/ui/components/habit_tile_component.dart';
 import 'package:habit_manager/core/ui/components/something_went_wrong.dart';
 import 'package:habit_manager/core/ui/consts/app_colors.dart';
@@ -85,16 +86,7 @@ class _YourHabitsScreenState extends State<YourHabitsScreen> {
                   : requestHasError
                   ? const SomethingWentWrong() 
                   : habits.isEmpty 
-                  ? const Center(
-                    child: Text(
-                      'Você ainda não possui nenhum habito cadastrado',
-                      style: TextStyle(
-                          color: AppColors.blue,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 28,
-                      ),
-                    ),
-                  )
+                  ? const EmptySpace()
                   : Expanded(
                     child: ListView.builder(
                         shrinkWrap: true,
